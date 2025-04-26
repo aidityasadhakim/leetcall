5e50ceb2d2edd31f540299e58f0fb0196f33df27
aidityas <aidityasadhakim250@gmail.com>
2025-04-26 11:26:15 +0700

    remove supabase template component

---
+++ app/api/test/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  return NextResponse.json(user, { status: 200 });
+}
